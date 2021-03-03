312f335b4b76a1be4533e949e497aec647182522
Alex K.D <78501883+md-alexkumar@users.noreply.github.com>
2021-03-03 13:49:15 +0530

    Removed unused imports

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import ForSync from "./forSync";
-import ForAsync from "./forAsync";
+// import ForAsync from "./forAsync";
 import "./index.css";
 
 ReactDOM.render(<ForSync />, document.getElementById("root"));
