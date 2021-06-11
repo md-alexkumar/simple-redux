241869947578318720471084de07b7ba60cc4ae9
Alex K.D <md.alexkumar@gmail.com>
2021-06-11 15:52:38 +0530

    Removed all the old files.

--- src/actionType.js
+++
@@ -1,5 +0,0 @@
-// Action Type
-export const INCREMENT = "INCREMENT";
-export const DECREMENT = "DECREMENT";
-export const INCREMENT_ASYNC = "INCREMENT_ASYNC";
-export const DECREMENT_ASYNC = "DECREMENT_ASYNC";
--- src/store.js
+++
@@ -1,6 +0,0 @@
-import { createStore } from "redux";
-
-import Counter from './reducer'
-
-// Connect the reducer to craete store
-export const store = createStore(Counter);
